096994a7bef2285ac19b23985090dce5bbca4e4e
heba thaher <67863629+hebathaher@users.noreply.github.com>
2021-04-20 14:26:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let my_info=Number(prompt('how old are you?'))||0;
 let second_value=Number(prompt('enter second value'))||0;
  let result;
-  result= Number(my_info)+Number(second_value); 
+  result= my_info+second_value; 
   alert (result); 
 
